9f5ad544f502a0df6f7545cb744f113b1f596817
Mark Fulton <mark.fulton@asurion.com>
2023-06-09 10:41:56 -0500

    feat(test): multiple test cases—inner for loop

--- solve.test.ts
+++ solve.test.ts
@@ -1,14 +1,14 @@
 import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
 import { solve } from "./solve.ts";
 
-Deno.test("84:1,3,4,5,10,25", () => {
-  const target = 84;
-  const operations = solve(target, [1, 3, 4, 5, 10, 25]);
-  assertEquals(operations?.at(-1)?.result, target);
-});
+const testCases = [
+  { target: 84, numbers: [1, 3, 4, 5, 10, 25] },
+  { target: 93, numbers: [1, 2, 3, 4, 5, 25] },
+];
 
-Deno.test("93:1,2,3,4,5,25", () => {
-  const target = 93;
-  const operations = solve(target, [1, 2, 3, 4, 5, 25]);
-  assertEquals(operations?.at(-1)?.result, target);
+Deno.test("testCases", () => {
+  for (const { target, numbers } of testCases) {
+    const operations = solve(target, numbers);
+    assertEquals(operations?.at(-1)?.result, target);
+  }
 });
