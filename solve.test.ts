92c590984c3aa18cf4f74805e93a92495edc41b0
Mark Fulton <mark.fulton@asurion.com>
2023-06-09 10:58:13 -0500

    feat(test): multiple test cases—macro template

--- solve.test.ts
+++ solve.test.ts
@@ -1,14 +1,15 @@
 import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
 import { solve } from "./solve.ts";
 
-function macro(t: Deno.TestContext) {
-  const [targetText, numbersText] = t.name.split(":");
-  const target = parseInt(targetText);
-  const numbers = numbersText.split(",").map((text) => parseInt(text));
-  const operations = solve(target, numbers);
-  assertEquals(operations?.at(-1)?.result, target);
+function expect(target: number, numbers: number[]) {
+  return function () {
+    const operations = solve(target, numbers);
+    assertEquals(operations?.at(-1)?.result, target);
+  };
 }
 
-Deno.test("84:1,3,4,5,10,25", macro);
-Deno.test("93:1,2,3,4,5,25", macro);
-Deno.test("112:1,2,3,4,5,25", macro);
+Deno.test("2023-06-09:pz0", expect(76, [10, 2, 1, 3, 4, 25]));
+Deno.test("2023-06-09:pz1", expect(131, [9, 7, 25, 2, 11, 3]));
+Deno.test("2023-06-09:pz2", expect(271, [3, 4, 6, 7, 8, 11]));
+Deno.test("2023-06-09:pz3", expect(312, [4, 7, 8, 9, 14, 20]));
+Deno.test("2023-06-09:pz4", expect(493, [9, 11, 14, 21, 23, 25]));
