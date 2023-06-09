6c66b780c1abc57b99c09845629cd841f2b359e5
Mark Fulton <mark.fulton@asurion.com>
2023-06-09 10:40:11 -0500

    feat(test): multiple test cases—duplicate code

--- solve.test.ts
+++ solve.test.ts
@@ -2,6 +2,13 @@ import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
 import { solve } from "./solve.ts";
 
 Deno.test("84:1,3,4,5,10,25", () => {
-  const operations = solve(84, [1, 3, 4, 5, 10, 25]);
-  assertEquals(operations?.[operations.length - 1].result, 84);
+  const target = 84;
+  const operations = solve(target, [1, 3, 4, 5, 10, 25]);
+  assertEquals(operations?.at(-1)?.result, target);
+});
+
+Deno.test("93:1,2,3,4,5,25", () => {
+  const target = 93;
+  const operations = solve(target, [1, 2, 3, 4, 5, 25]);
+  assertEquals(operations?.at(-1)?.result, target);
 });
