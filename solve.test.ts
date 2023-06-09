8277425bdc09a0c6f37ef287e3d0a09b8b2f3eda
Mark Fulton <mark.fulton@asurion.com>
2023-06-09 10:45:51 -0500

    feat(test): multiple test cases—macro style

--- solve.test.ts
+++ solve.test.ts
@@ -1,14 +1,14 @@
 import { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
 import { solve } from "./solve.ts";
 
-const testCases = [
-  { target: 84, numbers: [1, 3, 4, 5, 10, 25] },
-  { target: 93, numbers: [1, 2, 3, 4, 5, 25] },
-];
-
-for (const { target, numbers } of testCases) {
-  Deno.test(`${target}:${numbers}`, () => {
-    const operations = solve(target, numbers);
-    assertEquals(operations?.at(-1)?.result, target);
-  });
+function macro(t: Deno.TestContext) {
+  const [targetText, numbersText] = t.name.split(":");
+  const target = parseInt(targetText);
+  const numbers = numbersText.split(",").map((text) => parseInt(text));
+  const operations = solve(target, numbers);
+  assertEquals(operations?.at(-1)?.result, target);
 }
+
+Deno.test("84:1,3,4,5,10,25", macro);
+Deno.test("93:1,2,3,4,5,25", macro);
+Deno.test("112:1,2,3,4,5,25", macro);
